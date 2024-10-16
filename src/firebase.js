800cf256b3417f823aa2ce5db0e5a5ae12254f95
Davis K <davis.krasts@gmail.com>
2024-10-16 22:50:59 +0300

    feat: add firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -3,20 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import firebaseConfig from './firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
