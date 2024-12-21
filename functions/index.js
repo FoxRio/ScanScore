39e6c537ec90b253e3f7166ba822681f5577fa75
Dāvis Krasts <davis.krasts@gmail.com>
2024-12-21 13:54:01 +0200

    feat: firebase function working

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,44 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-const OpenAI = require('openai'); // Use require to load OpenAI module
+const OpenAI = require('openai');
+const admin = require('firebase-admin');
+const axios = require('axios');
+// const os = require('os');
+
+// Initialize Firebase Admin SDK if not already done
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Fetch the OpenAI API key from Firebase environment configuration
+// OpenAI endpoint
 const apiKey = functions.config().openai.key;
+const openai = new OpenAI({ apiKey });
 
-// Initialize OpenAI instance
-const openai = new OpenAI({
-  apiKey,
-});
+// async function checkDownloadToken(bucketName, filePath) {
+//   const bucket = admin.storage().bucket(bucketName);
+//   const file = bucket.file(filePath);
+
+//   try {
+//     const [metadata] = await file.getMetadata();
+//     const downloadTokens = metadata.metadata?.firebaseStorageDownloadTokens;
+
+//     if (downloadTokens) {
+//       console.log('Download token exists:', downloadTokens);
+//     } else {
+//       console.log('No download token available for the file.');
+//     }
+
+//     return downloadTokens || null;
+//   } catch (error) {
+//     console.error('Error fetching metadata:', error);
+//     return null;
+//   }
+// }
 
 app.post('/call-openai', async (req, res) => {
   try {
@@ -34,7 +59,7 @@ app.post('/call-openai', async (req, res) => {
     const systemPrompt = `You are a helpful assistant for a teacher. Your goal is to provide suggestions for a test named ${title}.
     The user provided this description about the test: ${description}.
     If you are asked to provide questions, ONLY provide the number of questions and answer multiple choice answers.
-    If you are unsure about the number of questions, default to one. Adjust the difficulty level based on the test's context. 
+    If you are unsure about the number of questions, default to one. Adjust the difficulty level based on the test's context.
     The conversation history is as follows: {${conversationHistory.join(' ')}}.`;
 
     const completion = await openai.chat.completions.create({
@@ -52,4 +77,43 @@ app.post('/call-openai', async (req, res) => {
     return res.status(500).json({ error: `Internal Server Error, ${error}` });
   }
 });
+
+app.post('/grade-test', async (req, res) => {
+  const {
+    correctAnswers, userId, fileName,
+  } = req.body;
+
+  if (!correctAnswers || !userId || !fileName) {
+    return res.status(400).json({ error: 'Missing required fields.' });
+  }
+  const imageToGradePath = `userUploads/${userId}/uploaded/${fileName}`;
+  const pythonApiUrl = 'https://europe-west1-scanscore-6cbf7.cloudfunctions.net/process_image';
+
+  try {
+    const bucket = admin.storage().bucket();
+    const file = bucket.file(imageToGradePath);
+
+    const [metadata] = await file.getMetadata();
+    console.log('File Metadata:', JSON.stringify(metadata, null, 2));
+    const downloadTokens = metadata.metadata?.firebaseStorageDownloadTokens;
+    if (!downloadTokens) {
+      return res.status(500).json({ error: 'No download token available for the file.' });
+    }
+    const downloadUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(imageToGradePath)}?alt=media&token=${downloadTokens}`;
+
+    const response = await axios.post(pythonApiUrl, {
+      image_url: downloadUrl,
+      correct_answers: correctAnswers,
+    });
+    console.log('Response:', response.data);
+    return res.status(200).send({
+      message: 'Test graded successfully',
+      response: response.data,
+    });
+  } catch (err) {
+    console.error('Error:', err);
+    return res.status(500).send({ error: 'Error processing the request', msg: err });
+  }
+});
+
 exports.api = functions.https.onRequest(app);
---
+++ functions/python_functions/main.py
@@ -0,0 +1,127 @@
+import functions_framework
+import cv2
+import requests
+import numpy as np
+from flask import jsonify, request
+import os
+import firebase_functions
+import logging
+
+logging.basicConfig(level=logging.DEBUG)  # You can change to INFO, WARNING, ERROR based on need
+logger = logging.getLogger()
+
+def download_image(image_url):
+    # Send a GET request to fetch the image from the URL
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        # Convert the image data to an array and then decode it to an OpenCV image
+        image_array = np.frombuffer(response.content, np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return image
+    else:
+        return None
+
+def findCheckedBoxes(image):
+    original = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Find contours and filter using contour area filtering to remove noise
+    cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    AREA_THRESHOLD = 10
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area < AREA_THRESHOLD:
+            cv2.drawContours(thresh, [c], -1, 0, -1)
+
+    # Repair checkbox horizontal and vertical walls
+    repair_kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 1))
+    repair = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, repair_kernel1, iterations=1)
+    repair_kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+    repair = cv2.morphologyEx(repair, cv2.MORPH_CLOSE, repair_kernel2, iterations=1)
+
+    # Detect checkboxes using shape approximation and aspect ratio filtering
+    checkbox_contours = []
+    cnts, _ = cv2.findContours(repair, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.035 * peri, True)
+        x,y,w,h = cv2.boundingRect(approx)
+        aspect_ratio = w / float(h)
+        if len(approx) == 4 and (0.8 <= aspect_ratio <= 1.2):
+            cv2.rectangle(original, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            checkbox_contours.append(c)
+
+    # print('Checkboxes:', len(checkbox_contours))
+
+    checkedBoxes = 0
+    isChecked = []
+    for contour in checkbox_contours:
+        # Create a binary mask for the specified contour
+        mask = np.zeros_like(repair)
+        cv2.drawContours(mask, [contour], 0, 255, thickness=cv2.FILLED)
+
+        # Apply the mask to the original image
+        result = cv2.bitwise_and(repair, mask)
+        # cv2.imshow('result', result)
+        # Count the number of white pixels in the masked area
+        white_pixel_count = np.sum(result == 255)
+
+        # print("Number of white pixels in the specified contour area:", white_pixel_count)
+        if white_pixel_count > 800:
+            isChecked.append(1)
+            # print("Is checked")
+            cv2.drawContours(original, [contour], 0, (0, 255, 0), 2)
+            checkedBoxes += 1
+            continue
+        isChecked.append(0)
+    # print(checkedBoxes)
+    # print(isChecked)
+    # cv2.imshow('thresh', thresh)
+    # cv2.imshow('repair', repair)
+    # cv2.imshow('original', original)
+    # In your Python script (findCheckedBoxes function)
+    cv2.imwrite('graded_image.jpg', original)  # Save the graded image
+    return isChecked
+
+def gradeFile(actualInput, expectedInput):
+    maxScore = len(expectedInput)
+    score = 0
+
+    # Print the actual input for debugging
+
+    # Reverse actualInput using slicing and compare with expectedInput
+    for actual, expected in zip(actualInput[::-1], expectedInput):
+        if actual == expected:
+            score += 1
+
+    result = [score, maxScore, score/maxScore * 100]
+    return result
+
+# Read the image using OpenCV
+@functions_framework.http
+def process_image(request):
+    print(f"Received full request: {request}")
+
+    request_json = request.get_json()
+
+    print(f"Received request JSON: {request_json}")
+
+    if not request_json:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    image_url = request_json.get('image_url')
+    correct_answers = request_json.get('correct_answers')
+
+    if not image_url or not correct_answers:
+        return jsonify({"error": "Missing required parameters"}), 400
+
+    image = download_image(image_url)
+    if image is None:
+        return jsonify({"error": "Failed to download image from URL"}), 400
+
+    # Proceed with image processing
+    evaluation = findCheckedBoxes(image)
+
+    return jsonify(gradeFile(evaluation, correct_answers))
